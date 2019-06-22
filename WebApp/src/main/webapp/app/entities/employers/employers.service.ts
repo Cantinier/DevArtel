3ef85dd630b0563364bfb30158ef02466613d807
Dante <stfaustoff@gmail.com>
2019-06-23 00:00:09 +0300

    change url

--- WebApp/src/main/webapp/app/entities/employers/employers.service.ts
+++ WebApp/src/main/webapp/app/entities/employers/employers.service.ts
@@ -11,28 +11,33 @@ type EntityArrayResponseType = HttpResponse<IEmployers[]>;
 
 @Injectable({ providedIn: 'root' })
 export class EmployersService {
-  public resourceUrl = SERVER_API_URL + 'api/employers';
+  public resourceUrl = SERVER_API_URL + 'services/dataservice/api/employers';
 
   constructor(protected http: HttpClient) {}
 
   create(employers: IEmployers): Observable<EntityResponseType> {
+    console.log(this.resourceUrl);    
     return this.http.post<IEmployers>(this.resourceUrl, employers, { observe: 'response' });
   }
 
   update(employers: IEmployers): Observable<EntityResponseType> {
+    console.log(this.resourceUrl);
     return this.http.put<IEmployers>(this.resourceUrl, employers, { observe: 'response' });
   }
 
   find(id: number): Observable<EntityResponseType> {
+    console.log(this.resourceUrl);
     return this.http.get<IEmployers>(`${this.resourceUrl}/${id}`, { observe: 'response' });
   }
 
   query(req?: any): Observable<EntityArrayResponseType> {
+    console.log(this.resourceUrl);
     const options = createRequestOption(req);
     return this.http.get<IEmployers[]>(this.resourceUrl, { params: options, observe: 'response' });
   }
 
   delete(id: number): Observable<HttpResponse<any>> {
+    console.log(this.resourceUrl);
     return this.http.delete<any>(`${this.resourceUrl}/${id}`, { observe: 'response' });
   }
 }
